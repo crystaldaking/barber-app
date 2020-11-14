f51dd9aa751f655f892d93140e79be701d51690c
crystal <crystal@crystalapps.by>
2020-11-14 21:40:32 +0300

    add ipad view

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/ipad',function (){
+    return view('ipad');
+});
 
 Route::namespace("App\Http\Controllers\Admin")->prefix("admin")->name("admin.")->middleware('can:manage-users')->group(function (){
     Route::resource("/users",UsersController::class,["except"=>["show","create","store"]]);
